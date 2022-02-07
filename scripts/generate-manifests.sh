8cc26dbf4b3f8a4660fcf214fd6f0c512f077a44
Stacky McStackface <github@stackable.de>
2022-02-07 19:53:51 +0000

    Update templated files to rev b5ebd49 (#111)
    Automatically created PR based on commit b5ebd49b2ed7fbaa53ced1cce021893ce4e47c87 in stackabletech/operator-templating repo.
    
    Original commit message:
    Publishing Artifacts step is skipped for bors staging branches but marked as required. (#63)
    
    This keeps bors from merging any prs.

--- scripts/generate-manifests.sh
+++ scripts/generate-manifests.sh
@@ -1,22 +1,22 @@
-#!/bin/bash
+#!/usr/bin/env bash
 # This script reads a Helm chart from deploy/helm/trino-operator and
 # generates manifest files into deploy/manifestss
 set -e
 
 tmp=$(mktemp -d ./manifests-XXXXX)
 
-helm template --output-dir $tmp \
+helm template --output-dir "$tmp" \
               --include-crds \
               --name-template trino-operator \
               deploy/helm/trino-operator
 
-for file in $(find $tmp -type f)
+while IFS= read -r -d '' file
 do
-    yq eval -i 'del(.. | select(has("app.kubernetes.io/managed-by")) | ."app.kubernetes.io/managed-by")' $file
-    yq eval -i 'del(.. | select(has("helm.sh/chart")) | ."helm.sh/chart")' $file
-    sed -i '/# Source: .*/d' $file
-done
+    yq eval -i 'del(.. | select(has("app.kubernetes.io/managed-by")) | ."app.kubernetes.io/managed-by")' "$file"
+    yq eval -i 'del(.. | select(has("helm.sh/chart")) | ."helm.sh/chart")' "$file"
+    sed -i '/# Source: .*/d' "$file"
+done <   <(find "$tmp" -type f)
 
-cp -r $tmp/trino-operator/*/* deploy/manifests/
+cp -r "$tmp"/trino-operator/*/* deploy/manifests/
 
-rm -rf $tmp
+rm -rf "$tmp"
