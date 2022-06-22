f6f661f7e654e477ec684df38e0cce1123002ba5
Nikolaus Winter <mail@nikolaus-winter.de>
2022-06-22 15:40:48 +0000

    removed CI files (#221)
    # Description
    
    Removed CI files

--- .ci/integration-tests/azure-aks/test.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-git clone -b "$GIT_BRANCH" https://github.com/stackabletech/trino-operator.git
-(cd trino-operator/ && ./scripts/run_tests.sh --parallel 1)
-exit_code=$?
-./operator-logs.sh trino > /target/trino-operator.log
-exit $exit_code
