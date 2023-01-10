cbe8413ed9a1a639b02ef2bdd1ccf8be3e685b69
Malte Sander <contact@maltesander.com>
2023-01-10 11:47:43 +0000

    Removed deploy crd folder (#363)
    # Description
    
    fixes https://github.com/stackabletech/trino-operator/issues/165
    
    
    
    Co-authored-by: Malte Sander <malte.sander.it@gmail.com>

--- rust/operator-binary/build.rs
+++ rust/operator-binary/build.rs
@@ -1,11 +1,3 @@
-use stackable_operator::crd::CustomResourceExt;
-use stackable_trino_crd::{catalog::TrinoCatalog, TrinoCluster};
-
-fn main() -> Result<(), stackable_operator::error::Error> {
+fn main() {
     built::write_built_file().expect("Failed to acquire build-time information");
-
-    TrinoCluster::write_yaml_schema("../../deploy/crd/trinocluster.crd.yaml")?;
-    TrinoCatalog::write_yaml_schema("../../deploy/crd/trinocatalog.crd.yaml")?;
-
-    Ok(())
 }
