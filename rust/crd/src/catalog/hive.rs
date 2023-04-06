1a009130cb354dde401af66d0f8bf29482b52160
Sebastian Bernauer <sebastian.bernauer@stackable.de>
2023-04-06 11:19:12 +0200

    Document that HMS also needs access to the dfs

--- rust/crd/src/catalog/hive.rs
+++ rust/crd/src/catalog/hive.rs
@@ -8,10 +8,12 @@ use stackable_operator::{
 #[derive(Clone, Debug, Deserialize, Eq, JsonSchema, PartialEq, Serialize)]
 #[serde(rename_all = "camelCase")]
 pub struct HiveConnector {
-    /// Mandatory connection to a Hive Metastore, which will be used as a storage for metadata
+    /// Mandatory connection to a Hive Metastore, which will be used as a storage for metadata.
     pub metastore: MetastoreConnection,
-    /// Connection to an S3 store
+    /// Connection to an S3 store.
+    /// Please make sure that the underlying Hive metastore also has access to the S3 store.
     pub s3: Option<S3ConnectionDef>,
-    /// Connection to an HDFS cluster
+    /// Connection to an HDFS cluster.
+    /// Please make sure that the underlying Hive metastore also has access to the HDFS.
     pub hdfs: Option<HdfsConnection>,
 }
