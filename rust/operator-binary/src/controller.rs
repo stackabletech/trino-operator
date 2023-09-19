fee4c7632c100d7065f5124b33200923c5decdce
Sebastian Bernauer <sebastian.bernauer@stackable.de>
2023-09-19 14:54:52 +0200

    refactor: Use new Duration struct from operator-rs (#474)
    * refactor: Use new Duration struct from operator-rs
    
    * changelog
    
    * Use stackable Duration for Action::requeue
    
    * remove humantime-serde

--- rust/operator-binary/src/controller.rs
+++ rust/operator-binary/src/controller.rs
@@ -21,6 +21,7 @@ use stackable_operator::{
         opa::OpaApiVersion, product_image_selection::ResolvedProductImage,
         rbac::build_rbac_resources,
     },
+    duration::Duration,
     k8s_openapi::{
         api::{
             apps::v1::{StatefulSet, StatefulSetSpec},
@@ -81,7 +82,6 @@ use std::{
     ops::Div,
     str::FromStr,
     sync::Arc,
-    time::Duration,
 };
 use strum::{EnumDiscriminants, IntoStaticStr};
 
@@ -1144,7 +1144,7 @@ fn build_rolegroup_service(
 }
 
 pub fn error_policy(_obj: Arc<TrinoCluster>, _error: &Error, _ctx: Arc<Ctx>) -> Action {
-    Action::requeue(Duration::from_secs(5))
+    Action::requeue(*Duration::from_secs(5))
 }
 
 /// Give a secret name and an optional key in the secret to use.
