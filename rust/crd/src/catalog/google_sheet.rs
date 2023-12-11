a2a10c7b257062c3479d228adf525e620abda222
Felix Hennig <felix.hennig@stackable.tech>
2023-12-11 16:21:56 +0100

    more catalog docs

--- rust/crd/src/catalog/google_sheet.rs
+++ rust/crd/src/catalog/google_sheet.rs
@@ -4,10 +4,10 @@ use stackable_operator::schemars::{self, JsonSchema};
 #[derive(Clone, Debug, Deserialize, Eq, JsonSchema, PartialEq, Serialize)]
 #[serde(rename_all = "camelCase")]
 pub struct GoogleSheetConnector {
-    /// Name of the secret containing the Google API JSON key file.
-    /// The key used from the Secret is `credentials`
+    /// The Secret containing the Google API JSON key file.
+    /// The key used from the Secret is `credentials`.
     pub credentials_secret: String,
-    /// Sheet ID of the spreadsheet, that contains the table mapping
+    /// Sheet ID of the spreadsheet, that contains the table mapping.
     pub metadata_sheet_id: String,
     /// Cache the contents of sheets.
     /// This is used to reduce Google Sheets API usage and latency.
@@ -17,8 +17,8 @@ pub struct GoogleSheetConnector {
 #[derive(Clone, Debug, Deserialize, Eq, JsonSchema, PartialEq, Serialize)]
 #[serde(rename_all = "camelCase")]
 pub struct GoogleSheetConnectorCache {
-    /// Maximum number of spreadsheets to cache, defaults to `1000`
+    /// Maximum number of spreadsheets to cache, defaults to 1000.
     pub sheets_data_max_cache_size: Option<String>,
-    /// How long to cache spreadsheet data or metadata, defaults to `5m`
+    /// How long to cache spreadsheet data or metadata, defaults to `5m`.
     pub sheets_data_expire_after_write: Option<String>,
 }
