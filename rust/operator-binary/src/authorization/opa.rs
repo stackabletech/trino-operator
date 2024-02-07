1a0a59d2b777c4f01ac5d061fd9cb07b8639cc99
Malte Sander <malte.sander.it@gmail.com>
2024-02-07 15:43:14 +0100

    add opa.allow-permission-management-operations for v438

--- rust/operator-binary/src/authorization/opa.rs
+++ rust/operator-binary/src/authorization/opa.rs
@@ -11,11 +11,15 @@ use stackable_operator::{
 use stackable_trino_crd::TrinoCluster;
 
 const PRODUCT_VERSIONS_WITH_OLD_AUTHORIZER: [&str; 1] = ["414"];
+// The version 428 was built based on the new Trino authorizer but from an unfinished upstream PR
+// e.g. the property `opa.allow-permission-management-operations` was not included.
+const PRODUCT_VERSIONS_WITH_INTERMEDIATE_AUTHORIZER: [&str; 1] = ["428"];
 
 pub struct TrinoOpaConfig {
     opa_authorizer_name: String,
     non_batched_connection_string: String,
     batched_connection_string: Option<String>,
+    allow_permission_management_operations: bool,
 }
 
 impl TrinoOpaConfig {
@@ -39,6 +43,27 @@ impl TrinoOpaConfig {
                 opa_authorizer_name: "tech.stackable.trino.opa.OpaAuthorizer".to_string(),
                 non_batched_connection_string,
                 batched_connection_string: None,
+                allow_permission_management_operations: false,
+            })
+        } else if PRODUCT_VERSIONS_WITH_INTERMEDIATE_AUTHORIZER
+            .contains(&resolved_product_image.product_version.as_str())
+        {
+            let non_batched_connection_string = opa_config
+                .full_document_url_from_config_map(client, trino, Some("allow"), OpaApiVersion::V1)
+                .await?;
+            let batched_connection_string = opa_config
+                .full_document_url_from_config_map(
+                    client,
+                    trino,
+                    Some("extended"),
+                    OpaApiVersion::V1,
+                )
+                .await?;
+            Ok(TrinoOpaConfig {
+                opa_authorizer_name: "opa".to_string(),
+                non_batched_connection_string,
+                batched_connection_string: Some(batched_connection_string),
+                allow_permission_management_operations: false,
             })
         } else {
             let non_batched_connection_string = opa_config
@@ -56,6 +81,7 @@ impl TrinoOpaConfig {
                 opa_authorizer_name: "opa".to_string(),
                 non_batched_connection_string,
                 batched_connection_string: Some(batched_connection_string),
+                allow_permission_management_operations: true,
             })
         }
     }
@@ -75,6 +101,12 @@ impl TrinoOpaConfig {
             config.insert(
                 "opa.policy.batched-uri".to_string(),
                 Some(batched_connection_string.clone()),
+            );
+        }
+        if self.allow_permission_management_operations {
+            config.insert(
+                "opa.allow-permission-management-operations".to_string(),
+                Some("true".to_string()),
             );
         }
         config
