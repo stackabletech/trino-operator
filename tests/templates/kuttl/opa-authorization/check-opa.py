e692d1370e42ed4ef89eee4ed67add2556e2ac13
Malte Sander <malte.sander.it@gmail.com>
2024-02-22 14:22:23 +0100

    simple test case working

--- tests/templates/kuttl/opa-authorization/check-opa.py
+++ tests/templates/kuttl/opa-authorization/check-opa.py
@@ -1,24 +1,28 @@
 #!/usr/bin/env python
+import argparse
 import pytest
 
 import trino
 from trino.exceptions import TrinoUserError
 
+import urllib3
+urllib3.disable_warnings()
+
 USER_PASSWORDS = {
     "admin": "admin",
-    "superset": "superset",
-    "data-analyst-1": "data-analyst-1",
-    "data-analyst-2": "data-analyst-2",
-    "data-analyst-3": "data-analyst-3",
-    "customer-1-user-1": "customer-1-user-1",
-    "customer-1-user-2": "customer-1-user-2",
-    "customer-2-user-1": "customer-2-user-1",
-    "customer-2-user-2": "customer-2-user-2",
+    #"superset": "superset",
+    #"data-analyst-1": "data-analyst-1",
+    #"data-analyst-2": "data-analyst-2",
+    #"data-analyst-3": "data-analyst-3",
+    #"customer-1-user-1": "customer-1-user-1",
+    #"customer-1-user-2": "customer-1-user-2",
+    #"customer-2-user-1": "customer-2-user-1",
+    #"customer-2-user-2": "customer-2-user-2",
 }
 
-def get_connection(username):
+def get_connection(username, namespace):
     conn = trino.dbapi.connect(
-        host="127.0.0.1",
+        host="trino-coordinator.{0}.svc.cluster.local".format(namespace),
         port=8443,
         user=username,
         http_scheme="https",
@@ -32,14 +36,14 @@ def run(connection, query):
     cursor.execute(query)
     return cursor.fetchall()
 
-def test_can_execute_queries():
+def test_can_execute_queries(namespace):
     """Everyone should be able to execute queries"""
     for username in USER_PASSWORDS.keys():
-        connection = get_connection(username)
+        connection = get_connection(username, namespace)
         assert run(connection, "select 42")[0][0] == 42
 
-def test_admin_access():
-    admin = get_connection("admin")
+def test_admin_access(namespace):
+    admin = get_connection("admin", namespace)
     assert run(admin, "show catalogs")[:][:] == [["lakehouse"],["system"],["tpcds"],["tpch"]]
     assert run(admin, "show schemas in system")[:][:] == [["information_schema"],["jdbc"],["metadata"],["runtime"]]
     assert run(admin, "show schemas in tpcds")[:][:] == [["information_schema"],["sf1"],["sf10"],["sf100"],["sf1000"],["sf10000"],["sf100000"],["sf300"],["sf3000"],["sf30000"],["tiny"]]
@@ -53,3 +57,22 @@ def test_data_analyst_1_access():
         with pytest.raises(TrinoUserError) as e:
             run(admin, "show schemas in tpcds")
         assert run(admin, "show schemas in tpch")[:][:] == [["information_schema"],["sf1"],["sf100"],["sf1000"],["sf10000"],["sf100000"],["sf300"],["sf3000"],["sf30000"],["tiny"]]
+
+
+def main():
+    # Construct an argument parser
+    all_args = argparse.ArgumentParser()
+    # Add arguments to the parser
+    all_args.add_argument(
+        "-n", "--namespace", required=True, help="Namespace the test is running in"
+    )
+
+    args = vars(all_args.parse_args())
+    namespace = args["namespace"]
+
+    test_can_execute_queries(namespace)
+
+    test_admin_access(namespace)
+
+if __name__ == "__main__":
+    main()
