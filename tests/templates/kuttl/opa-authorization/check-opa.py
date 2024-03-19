bbb71729ca949510b68ff7275e7ffe4b6aa5cc92
Siegfried Weber <mail@siegfriedweber.net>
2024-03-19 15:55:19 +0100

    Fix flake8 warnings

--- tests/templates/kuttl/opa-authorization/check-opa.py
+++ tests/templates/kuttl/opa-authorization/check-opa.py
@@ -28,11 +28,11 @@ TEST_DATA = [
             "password": "admin",
         },
         "tests": [
-            ### CATALOG ###
+            # ## CATALOG ##
             # ExecuteQuery, FilterCatalogs
             {
                 "query": "SHOW CATALOGS",
-                "expected": [["iceberg"],["lakehouse"],["system"],["tpcds"],["tpch"]],
+                "expected": [["iceberg"], ["lakehouse"], ["system"], ["tpcds"], ["tpch"]],
             },
             # ExecuteQuery, FilterCatalogs, ImpersonateUser
             {
@@ -46,16 +46,16 @@ TEST_DATA = [
                 # The requests are authorized, just a fake property
                 "error": "Session property 'iceberg.test' does not exist",
             },
-            ### SCHEMA ###
+            # ## SCHEMA ##
             # ExecuteQuery, AccessCatalog, ShowSchemas, SelectFromColumns, FilterCatalogs, FilterSchemas
             {
                 "query": "SHOW SCHEMAS in tpch",
-                "expected": [["information_schema"],["sf1"],["sf100"],["sf1000"],["sf10000"],["sf100000"],["sf300"],["sf3000"],["sf30000"],["tiny"]],
+                "expected": [["information_schema"], ["sf1"], ["sf100"], ["sf1000"], ["sf10000"], ["sf100000"], ["sf300"], ["sf3000"], ["sf30000"], ["tiny"]],
             },
             # ExecuteQuery, AccessCatalog, ShowSchemas, SelectFromColumns, FilterCatalogs, FilterSchemas
             {
                 "query": "SHOW SCHEMAS in system",
-                "expected": [["information_schema"],["jdbc"],["metadata"],["runtime"]],
+                "expected": [["information_schema"], ["jdbc"], ["metadata"], ["runtime"]],
             },
             # ExecuteQuery, AccessCatalog, CreateSchema
             {
@@ -66,7 +66,7 @@ TEST_DATA = [
             {
                 "query": "SHOW CREATE SCHEMA iceberg.test",
                 "expected": [["CREATE SCHEMA iceberg.test\nAUTHORIZATION USER admin\nWITH (\n   location = 's3a://trino/iceberg/test'\n)"]],
-            },            
+            },
             # ExecuteQuery, AccessCatalog, SetSchemaAuthorization
             {
                 "query": "ALTER SCHEMA iceberg.test SET AUTHORIZATION admin",
@@ -83,12 +83,12 @@ TEST_DATA = [
                 "query": "ALTER SCHEMA iceberg.test RENAME TO test1",
                 # The requests are authorized, just the hive connector does not support this
                 "error": "Hive metastore does not support renaming schemas",
-            },            
-            ### TABLE ###
+            },
+            # ## TABLE ##
             # ExecuteQuery, AccessCatalog, ShowTables, SelectFromColumns, FilterCatalogs, FilterTables
             {
                 "query": "SHOW TABLES in tpch.sf1",
-                "expected": [["customer"],["lineitem"],["nation"],["orders"],["part"],["partsupp"],["region"],["supplier"]],
+                "expected": [["customer"], ["lineitem"], ["nation"], ["orders"], ["part"], ["partsupp"], ["region"], ["supplier"]],
             },
             # ExecuteQuery, AccessCatalog, CreateTable
             {
@@ -100,7 +100,7 @@ TEST_DATA = [
                 "query": "COMMENT ON TABLE iceberg.test.test IS 'This is a test table!'",
                 "expected": [],
             },
-            # ExecuteQuery, AccessCatalog, SetColumnComment 
+            # ExecuteQuery, AccessCatalog, SetColumnComment
             {
                 "query": "COMMENT ON COLUMN iceberg.test.test.col1 IS 'This is a column comment!'",
                 "expected": [],
@@ -115,7 +115,7 @@ TEST_DATA = [
             {
                 "query": "ALTER TABLE iceberg.test.test ADD COLUMN col3 bigint",
                 "expected": [],
-            },            
+            },
             # ExecuteQuery, AccessCatalog, RenameColumn
             {
                 "query": "ALTER TABLE iceberg.test.test RENAME COLUMN col3 TO col_renamed",
@@ -129,7 +129,7 @@ TEST_DATA = [
             # ExecuteQuery, AccessCatalog, ShowColumns, SelectFromColumns, FilterCatalogs, FilterTables, FilterColumns
             {
                 "query": "DESCRIBE iceberg.test.test",
-                "expected": [["col1", "bigint", "", "This is a column comment!"],["col2", "bigint", "", ""]],
+                "expected": [["col1", "bigint", "", "This is a column comment!"], ["col2", "bigint", "", ""]],
             },
             # ExecuteQuery, AccessCatalog, InsertIntoTable
             {
@@ -147,7 +147,7 @@ TEST_DATA = [
                 "query": "UPDATE iceberg.test.test SET col1=1 WHERE col1>1",
                 # 2 rows updated
                 "expected": [[2]],
-            },            
+            },
             # ExecuteQuery, AccessCatalog, SelectFromColumns, DeleteFromTable
             {
                 "query": "DELETE FROM iceberg.test.test WHERE col2=6",
@@ -164,13 +164,13 @@ TEST_DATA = [
                 "query": "TRUNCATE TABLE iceberg.test.test2",
                 # The requests are authorized, just the hive connector does not support this
                 "error": "This connector does not support truncating tables",
-            },            
+            },
             # ExecuteQuery, AccessCatalog, DropTable
             {
                 "query": "DROP TABLE iceberg.test.test2",
                 "expected": [],
             },
-            ### VIEW ###
+            # ## VIEW ##
             # ExecuteQuery, AccessCatalog, SelectFromColumns, CreateView
             {
                 "query": "CREATE VIEW iceberg.test.v_customer AS SELECT name, address FROM tpch.sf1.customer",
@@ -184,9 +184,9 @@ TEST_DATA = [
             # ExecuteQuery, AccessCatalog, SetViewAuthorization
             {
                 "query": "ALTER VIEW iceberg.test.v_customer SET AUTHORIZATION admin",
-                # The requests are authorized, just the hive connector does not support this                
+                # The requests are authorized, just the hive connector does not support this
                 "error": "This connector does not support setting an owner on a table",
-            }, 
+            },
             # ExecuteQuery, AccessCatalog, RenameView
             {
                 "query": "ALTER VIEW iceberg.test.v_customer RENAME TO iceberg.test.v_customer_renamed",
@@ -202,12 +202,12 @@ TEST_DATA = [
                 "query": "DROP VIEW iceberg.test.v_customer_renamed",
                 "expected": [],
             },
-            ### MATERIALIZED VIEW ###
-            # ExecuteQuery, AccessCatalog, SelectFromColumns, CreateMaterializedView  
+            # ## MATERIALIZED VIEW ##
+            # ExecuteQuery, AccessCatalog, SelectFromColumns, CreateMaterializedView
             {
                 "query": "CREATE MATERIALIZED VIEW iceberg.test.mv_customer AS SELECT name, address FROM tpch.sf1.customer",
                 "expected": [],
-            },            
+            },
             # ExecuteQuery, AccessCatalog, RefreshMaterializedView
             {
                 "query": "REFRESH MATERIALIZED VIEW iceberg.test.mv_customer",
@@ -230,7 +230,7 @@ TEST_DATA = [
                 "query": "DROP MATERIALIZED VIEW iceberg.test.mv_customer_renamed",
                 "expected": [],
             },
-            ### FUNCTIONS ###
+            # ## FUNCTIONS ##
             # ExecuteQuery, AccessCatalog, ShowFunctions
             {
                 "query": "SHOW FUNCTIONS IN iceberg.test",
@@ -248,36 +248,36 @@ TEST_DATA = [
                 # The requests are authorized, was not created in the step above due to hive connector not supporting this
                 "error": "Function not found",
             },
-            ### SystemSessionProperties ###
+            # ## SystemSessionProperties ##
             # ExecuteQuery, SetSystemSessionProperty
             {
                 "query": "SET SESSION optimize_hash_generation = true",
                 "expected": [],
             },
-            ### PROCEDURES ###
+            # ## PROCEDURES ##
             # ExecuteQuery, AccessCatalog, ExecuteProcedure
             {
                 "query": "CALL system.runtime.kill_query(query_id => '20151207_215727_00146_tx3nr', message => 'Using too many resources')",
-                # The requests are authorized, task did not exist..              
+                # The requests are authorized, task did not exist..
                 "error": "Target query not found: 20151207_215727_00146_tx3nr",
             },
-            ### QUERIES ###
+            # ## QUERIES ##
             # ExecuteQuery, AccessCatalog, SelectFromColumns, FilterViewQueryOwnedBy
             {
                 "query": "SELECT COUNT(*) FROM (SELECT * FROM system.runtime.queries LIMIT 1)",
                 "expected": [[1]],
             },
 
-            ### CLEAN UP ###
+            # ## CLEAN UP ##
             # ExecuteQuery, AccessCatalog, DropSchema
             {
                 "query": "DROP SCHEMA iceberg.test",
                 "expected": [],
-            },                        
+            },
         ]
     },
     {
-        # User lakehouse can: 
+        # User lakehouse can:
         # - execute queries
         # - only access read-only lakehouse catalog
         # - access schemas tiny and sf1 in catalog lakehouse
@@ -294,7 +294,7 @@ TEST_DATA = [
             },
             {
                 "query": "SHOW SCHEMAS IN lakehouse",
-                "expected": [["information_schema"],["sf1"],["tiny"]],
+                "expected": [["information_schema"], ["sf1"], ["tiny"]],
             },
             {
                 "query": "SHOW SCHEMAS IN tpch",
@@ -303,7 +303,7 @@ TEST_DATA = [
             {
                 "query": "SHOW TABLES IN lakehouse.sf1",
                 "expected": [["customer"]],
-            },            
+            },
             {
                 "query": "SELECT name FROM lakehouse.tiny.customer ORDER BY name LIMIT 1",
                 "expected": [["Customer#000000001"]],
@@ -341,7 +341,7 @@ TEST_DATA = [
         ]
     },
     {
-        # User iceberg can: 
+        # User iceberg can:
         # - execute queries
         # - access iceberg catalog
         # - create, drop and access all schemas
@@ -369,12 +369,12 @@ TEST_DATA = [
             },
             {
                 "query": "SELECT * FROM iceberg.test2.test",
-                "expected": [[1],[2]],
+                "expected": [[1], [2]],
             },
             {
                 "query": "UPDATE iceberg.test2.test SET test=3 WHERE test=2",
                 "error": "Access Denied: Cannot update columns",
-            },                      
+            },
             {
                 "query": "DELETE FROM iceberg.test2.test WHERE test=2",
                 "expected": [[1]],
@@ -386,17 +386,17 @@ TEST_DATA = [
             {
                 "query": "DROP SCHEMA iceberg.test2",
                 "expected": [],
-            },   
+            },
         ]
-    }    
+    }
 ]
+
 
 class TestOpa:
 
     def __init__(self, test_data, namespace):
         self.data = test_data
         self.namespace = namespace
-
 
     def run(self):
         for test_case in self.data:
@@ -430,24 +430,22 @@ class TestOpa:
         timestamp = datetime.utcnow().isoformat(sep=' ', timespec='milliseconds')
         print(f'[{timestamp}] - {user:20s} -> {query}')
 
-
     def run_query(connection, query):
         cursor = connection.cursor()
         cursor.execute(query)
         return cursor.fetchall()
 
-
     def get_connection(username, password, namespace, impersonation=None):
         connection = trino.dbapi.connect(
             host="trino-coordinator.{0}.svc.cluster.local".format(namespace),
             port=8443,
-            user=impersonation if impersonation != None else username,
+            user=impersonation if impersonation is not None else username,
             http_scheme="https",
             auth=trino.auth.BasicAuthentication(username, password),
             verify=False,
         )
         return connection
-    
+
 
 def main():
     # Construct an argument parser
