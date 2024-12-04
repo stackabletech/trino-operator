1e74dab3dd1b791747ba8b53409dae1e75883792
Razvan-Daniel Mihai <84674+razvan@users.noreply.github.com>
2024-12-04 11:05:58 +0100

    rustfmt

--- rust/crd/src/lib.rs
+++ rust/crd/src/lib.rs
@@ -432,7 +432,7 @@ pub struct TrinoConfig {
     /// Time period Pods have to gracefully shut down, e.g. `30m`, `1h` or `2d`. Consult the operator documentation for details.
     #[fragment_attrs(serde(default))]
     pub graceful_shutdown_timeout: Option<Duration>,
- 
+
     /// Request secret (currently only autoTls certificates) lifetime from the secret operator, e.g. `7d`, or `30d`.
     /// This can be shortened by the `maxCertificateLifetime` setting on the SecretClass issuing the TLS certificate.
     #[fragment_attrs(serde(default))]
