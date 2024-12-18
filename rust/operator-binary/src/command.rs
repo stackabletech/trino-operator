26526b300b1e64204b5fb11cebbeb4a9683a8c13
Razvan-Daniel Mihai <84674+razvan@users.noreply.github.com>
2024-12-18 21:08:01 +0100

    feat: run containerdebug in the background (#687)
    * feat: run containerdebug in the background
    
    * fix container name
    
    * fix containerdebug log dir
    
    * fix after running the tests
    
    * reintroduce env var
    
    * factor out ctnrdebug test
    
    * Apply Java heap size limit to Keycloak in opa-authorization test
    
    It was hitting against the container memory limits, and getting oomkilled.
    
    ---------
    
    Co-authored-by: Natalie Klestrup Röijezon <nat@nullable.se>

--- rust/operator-binary/src/command.rs
+++ rust/operator-binary/src/command.rs
@@ -118,6 +118,7 @@ pub fn container_trino_args(
 {COMMON_BASH_TRAP_FUNCTIONS}
 {remove_vector_shutdown_file_command}
 prepare_signal_handlers
+containerdebug --output={STACKABLE_LOG_DIR}/containerdebug-state.json --loop &
 bin/launcher run --etc-dir={RW_CONFIG_DIR_NAME} --data-dir={DATA_DIR_NAME} &
 wait_for_termination $!
 {create_vector_shutdown_file_command}
