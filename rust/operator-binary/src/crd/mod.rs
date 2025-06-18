e9ce6e2b00a789b0a8f88bd9fd1a525d4a93f291
Malte Sander <malte.sander.it@gmail.com>
2025-06-18 11:54:50 +0200

    fix listener examples / comments

--- rust/operator-binary/src/crd/mod.rs
+++ rust/operator-binary/src/crd/mod.rs
@@ -845,7 +845,7 @@ impl v1alpha1::TrinoCluster {
             }))
     }
 
-    /// Returns the headless rolegroup service name `simple-trino-coordinator-default-<HEADLESS_SERVICE_SUFFIX>`.
+    /// Returns the metrics rolegroup service name `simple-trino-coordinator-default-<METRICS_SERVICE_SUFFIX>`.
     pub fn rolegroup_metrics_service_name(role_group_ref_object_name: &str) -> String {
         format!("{}-{}", role_group_ref_object_name, METRICS_SERVICE_SUFFIX)
     }
