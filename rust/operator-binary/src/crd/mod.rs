186aa8ed06122e68783cbb82a48f7800713dc47b
Malte Sander <malte.sander.it@gmail.com>
2025-06-24 15:59:49 +0200

    typo

--- rust/operator-binary/src/crd/mod.rs
+++ rust/operator-binary/src/crd/mod.rs
@@ -940,7 +940,7 @@ impl v1alpha1::TrinoCluster {
     }
 }
 
-/// Returns the metrics rolegroup service name `<cluster>-<role->-<rolegroup>-<METRICS_SERVICE_SUFFIX>`.
+/// Returns the metrics rolegroup service name `<cluster>-<role>-<rolegroup>-<METRICS_SERVICE_SUFFIX>`.
 pub fn rolegroup_metrics_service_name(role_group_ref_object_name: &str) -> String {
     format!("{role_group_ref_object_name}-{METRICS_SERVICE_SUFFIX}")
 }
