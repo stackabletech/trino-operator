a0a5281b68156ce418c8aefe8bb3c74758e905c2
Razvan-Daniel Mihai <84674+razvan@users.noreply.github.com>
2026-04-07 17:27:16 +0200

    use op-rs version of internal secrets

--- rust/operator-binary/src/controller.rs
+++ rust/operator-binary/src/controller.rs
@@ -27,10 +27,10 @@ use stackable_operator::{
             volume::{SecretFormat, SecretOperatorVolumeSourceBuilder, VolumeBuilder},
         },
     },
-    client::Client,
     cluster_resources::{ClusterResourceApplyStrategy, ClusterResources},
     commons::{
         product_image_selection::{self, ResolvedProductImage},
+        random_secret_creation,
         rbac::build_rbac_resources,
         secret_class::SecretClassVolumeProvisionParts,
     },
@@ -41,14 +41,13 @@ use stackable_operator::{
             apps::v1::{StatefulSet, StatefulSetSpec},
             core::v1::{
                 ConfigMap, ConfigMapVolumeSource, ContainerPort, EnvVar, EnvVarSource, ExecAction,
-                HTTPGetAction, Probe, Secret, SecretKeySelector, Volume,
+                HTTPGetAction, Probe, SecretKeySelector, Volume,
             },
         },
         apimachinery::pkg::{apis::meta::v1::LabelSelector, util::intstr::IntOrString},
     },
     kube::{
-        Api, Resource, ResourceExt,
-        api::DeleteParams,
+        Resource, ResourceExt,
         core::{DeserializeGuard, error_boundary},
         runtime::{controller::Action, reflector::ObjectRef},
     },
@@ -392,6 +391,11 @@ pub enum Error {
         "client spooling protocol is not supported for Trino version {product_version}"
     ))]
     ClientSpoolingProtocolTrinoVersion { product_version: String },
+
+    #[snafu(display("failed to create internal secret"))]
+    InvalidInternalSecret {
+        source: random_secret_creation::Error,
+    },
 }
 
 type Result<T, E = Error> = std::result::Result<T, E>;
@@ -544,25 +548,27 @@ pub async fn reconcile_trino(
         None => None,
     };
 
-    create_random_secret_if_not_exists(
+    random_secret_creation::create_random_secret_if_not_exists(
         &shared_internal_secret_name(trino),
         ENV_INTERNAL_SECRET,
         512,
         trino,
         client,
     )
-    .await?;
+    .await
+    .context(InvalidInternalSecretSnafu)?;
 
     // This secret is created even if spooling is not configured.
     // Trino currently requires the secret to be exactly 256 bits long.
-    create_random_secret_if_not_exists(
+    random_secret_creation::create_random_secret_if_not_exists(
         &shared_spooling_secret_name(trino),
         ENV_SPOOLING_SECRET,
         32,
         trino,
         client,
     )
-    .await?;
+    .await
+    .context(InvalidInternalSecretSnafu)?;
 
     let mut sts_cond_builder = StatefulSetConditionBuilder::default();
 
@@ -1532,112 +1538,12 @@ fn build_recommended_labels<'a>(
     }
 }
 
-/// This function creates a random Secret if it doesn't already exist.
-///
-/// As this function generates random Secret contents, if the Secret already exists, it will *not*
-/// be patched, as otherwise we would generate new Secret contents on every reconcile. Which would
-/// in turn cause Pods restarts, which would cause reconciles ;)
-///
-/// However, there is one special handling needed:
-///
-/// We can't mark Secrets as immutable, as this caused problems, see https://github.com/stackabletech/issues/issues/843.
-/// As Secrets have been created as immutable up to SDP release 26.3.0, we need to delete the, to be
-/// able to re-create them as mutable. This function detects old (immutable) Secrets and re-creates
-/// them as mutable. The contents of the Secret will be kept to prevent unnecessary Secret content
-/// changes.
-async fn create_random_secret_if_not_exists(
-    secret_name: &str,
-    secret_key: &str,
-    secret_byte_size: usize,
-    trino: &v1alpha1::TrinoCluster,
-    client: &Client,
-) -> Result<()> {
-    let secret_namespace = trino.namespace().context(ObjectHasNoNamespaceSnafu)?;
-    let existing_secret = client
-        .get_opt::<Secret>(secret_name, &secret_namespace)
-        .await
-        .context(FailedToRetrieveInternalSecretSnafu)?;
-
-    match existing_secret {
-        Some(
-            existing_secret @ Secret {
-                immutable: Some(true),
-                ..
-            },
-        ) => {
-            tracing::info!(
-                k8s.secret.name = secret_name,
-                k8s.secret.namespace = secret_namespace,
-                "Old (immutable) Secret detected, re-creating it to be able to make it mutable. The contents will stay the same."
-            );
-            Api::<Secret>::namespaced(client.as_kube_client(), &secret_namespace)
-                .delete(secret_name, &DeleteParams::default())
-                .await
-                .context(DeleteInternalSecretSnafu)?;
-
-            let mut mutable_secret = existing_secret;
-            mutable_secret.immutable = Some(false);
-            // Prevent "ApiError: resourceVersion should not be set on objects to be created"
-            mutable_secret.metadata.resource_version = None;
-
-            client
-                .create(&mutable_secret)
-                .await
-                .context(ApplyInternalSecretSnafu)?;
-
-            // Note: restart-controller will restart all Trino (coordinator and worker) Pods as the
-            // mounted Secret changed.
-        }
-        Some(_) => {
-            tracing::debug!(
-                k8s.secret.name = secret_name,
-                k8s.secret.namespace = secret_namespace,
-                "Existing (mutable) Secret detected, nothing to do"
-            );
-        }
-        None => {
-            tracing::info!(
-                k8s.secret.name = secret_name,
-                k8s.secret.namespace = secret_namespace,
-                "Random Secret missing, creating it"
-            );
-            let secret = Secret {
-                metadata: ObjectMetaBuilder::new()
-                    .name(secret_name)
-                    .namespace_opt(trino.namespace())
-                    .ownerreference_from_resource(trino, None, Some(true))
-                    .context(ObjectMissingMetadataForOwnerRefSnafu)?
-                    .build(),
-                string_data: Some(BTreeMap::from([(
-                    secret_key.to_string(),
-                    get_random_base64(secret_byte_size),
-                )])),
-                ..Secret::default()
-            };
-            client
-                .create(&secret)
-                .await
-                .context(ApplyInternalSecretSnafu)?;
-        }
-    }
-
-    Ok(())
-}
-
 fn shared_internal_secret_name(trino: &v1alpha1::TrinoCluster) -> String {
     format!("{}-internal-secret", trino.name_any())
 }
 
 fn shared_spooling_secret_name(trino: &v1alpha1::TrinoCluster) -> String {
     format!("{}-spooling-secret", trino.name_any())
-}
-
-// TODO: Maybe switch to something non-openssl.
-// See https://github.com/stackabletech/airflow-operator/pull/686#discussion_r2348354468 (which is currently under discussion)
-fn get_random_base64(byte_size: usize) -> String {
-    let mut buf: Vec<u8> = vec![0; byte_size];
-    openssl::rand::rand_bytes(&mut buf).unwrap();
-    openssl::base64::encode_block(&buf)
 }
 
 fn container_ports(trino: &v1alpha1::TrinoCluster) -> Vec<ContainerPort> {
