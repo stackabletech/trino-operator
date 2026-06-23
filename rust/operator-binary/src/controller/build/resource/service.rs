d7c35e656e68bf6e1d213bcf4229e2d81fcbc1b0
Malte Sander <malte.sander.it@gmail.com>
2026-06-23 16:11:31 +0200

    chore: replace promethues label & annotations with op-rs v2 helper

--- rust/operator-binary/src/controller/build/resource/service.rs
+++ rust/operator-binary/src/controller/build/resource/service.rs
@@ -2,7 +2,8 @@ use std::collections::BTreeMap;
 
 use stackable_operator::{
     k8s_openapi::api::core::v1::{Service, ServicePort, ServiceSpec},
-    kvp::{Annotations, Labels},
+    kvp::Labels,
+    v2::builder::service::{Scheme, Scraping, prometheus_annotations, prometheus_labels},
 };
 
 use crate::{
@@ -60,8 +61,13 @@ pub fn build_rolegroup_metrics_service(
                     .to_string(),
                 recommended_labels.clone(),
             )
-            .with_labels(prometheus_labels())
-            .with_annotations(prometheus_annotations())
+            .with_labels(prometheus_labels(&Scraping::Enabled))
+            .with_annotations(prometheus_annotations(
+                &Scraping::Enabled,
+                &Scheme::Http,
+                "/metrics",
+                &METRICS_PORT,
+            ))
             .build(),
         spec: Some(ServiceSpec {
             // Internal communication does not need to be exposed
@@ -95,24 +101,4 @@ fn metrics_service_ports() -> Vec<ServicePort> {
         protocol: Some("TCP".to_string()),
         ..ServicePort::default()
     }]
-}
-
-/// Common labels for Prometheus
-fn prometheus_labels() -> Labels {
-    Labels::try_from([("prometheus.io/scrape", "true")]).expect("should be a valid label")
-}
-
-/// Common annotations for Prometheus
-///
-/// These annotations can be used in a ServiceMonitor.
-///
-/// see also <https://github.com/prometheus-community/helm-charts/blob/prometheus-27.32.0/charts/prometheus/values.yaml#L983-L1036>
-fn prometheus_annotations() -> Annotations {
-    Annotations::try_from([
-        ("prometheus.io/path".to_owned(), "/metrics".to_owned()),
-        ("prometheus.io/port".to_owned(), METRICS_PORT.to_string()),
-        ("prometheus.io/scheme".to_owned(), "http".to_owned()),
-        ("prometheus.io/scrape".to_owned(), "true".to_owned()),
-    ])
-    .expect("should be valid annotations")
 }
